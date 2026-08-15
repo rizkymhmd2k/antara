5f29eb672952b734ed6e02ae7c2fdfecf5c093c1
Rizky <muhammadrizky1155@gmail.com>
2026-08-15 20:24:11 +0800

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  site: "https://antarawealth.pages.dev",
+  integrations: [icon(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
